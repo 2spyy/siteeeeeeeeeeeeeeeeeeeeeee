9e2659417976ddcf66bd07107672f58ea95d4be9
2spyy <107276066+2spyy@users.noreply.github.com>
2022-07-20 22:32:48 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// TailwindCSS Framework - Initialization
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        sweetBlack: "#E74C3C",
+        shinyGray: "#E74C3C",
+        beautyPurple: "#E74C3C",
+        blurple: "#E74C3C"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
